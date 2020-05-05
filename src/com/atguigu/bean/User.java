cf8ce4ef0d43b151a2a73c2c5a90bf90bbbd22ad
mingjiu <ymj@123.com>
2020-05-05 15:59:22 +0800

    1111

--- src/com/atguigu/bean/User.java
+++ src/com/atguigu/bean/User.java
@@ -6,6 +6,6 @@ package com.atguigu.bean;
  */
 public class User {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello,111q");
     }
 }
