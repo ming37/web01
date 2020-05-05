101738945482b6b281af3c8a08c649aa67c3cb22
mingjiu <ymj@123.com>
2020-05-05 15:55:21 +0800

    Initial commit

---
+++ src/com/atguigu/bean/User.java
@@ -0,0 +1,11 @@
+package com.atguigu.bean;
+
+/**
+ * @author Administrator
+ * @create 2020-05-05 15:50
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
